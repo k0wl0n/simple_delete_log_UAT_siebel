853370b165afbb3fd09f443699067355a20949ce
Wahyu anggana <wahyuanggana1@gmail.com>
2019-01-07 11:11:19 +0700

    init

---
+++ JTN/remove_log_sblsitsvr1.crm.jtn.telkom.co.id_UAT.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR=/opt/Siebel16/ses/siebsrvr/enterprises/SBLSIT_JTN/sblsitsvr1/logarchive
+rm -rf $DIR/*
+
+if [ -n "$(find "$DIR" -maxdepth 0 -type d -empty 2>/dev/null)" ]; then
+    echo 'log berhasil di hapus'
+else
+    echo "log gagal di hapus"
+fi
---
+++ STL/remove_log_sblsitsvr2.crm.stl.telkom.co.id_UAT.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR=/opt/Siebel16/ses/siebsrvr/enterprises/SBLSIT_STL/sblsitsvr2/logarchive
+rm -rf $sampledir/*
+
+if [ -n "$(find "$DIR" -maxdepth 0 -type d -empty 2>/dev/null)" ]; then
+    echo 'log berhasil di hapus'
+else
+    echo "log gagal di hapus"
+fi
