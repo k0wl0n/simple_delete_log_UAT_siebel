89d5f29a6823c1ac94b35eaee247b302c17452c3
Wahyu anggana <wahyuanggana1@gmail.com>
2019-01-07 15:17:32 +0700

    update bugs critical

--- STL/remove_log_sblsitsvr2.crm.stl.telkom.co.id_UAT.sh
+++ STL/remove_log_sblsitsvr2.crm.stl.telkom.co.id_UAT.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR=/opt/Siebel16/ses/siebsrvr/enterprises/SBLSIT_STL/sblsitsvr2/logarchive
-rm -rf $sampledir/*
+rm -rf $DIR/*
 
 if [ -n "$(find "$DIR" -maxdepth 0 -type d -empty 2>/dev/null)" ]; then
     echo 'log berhasil di hapus'
